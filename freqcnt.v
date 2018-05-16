dc65e75079e6ad4c02b48f4b8305600a6644257f
skytll <18774330113@163.com>
2018-05-17 07:40:16 +0800

    might work

--- freqcnt.v
+++ freqcnt.v
@@ -1,40 +1,37 @@
 module freqcnt(sysclk, rst, range, testmode, freq0, freq1, freq2, freq3, rangedisp);
 input sysclk, rst, range;
-inout [1:0] testmode;
+input [1:0] testmode;
 output [3:0] freq0, freq1, freq2, freq3;
 output rangedisp;
 wire [3:0] freq0, freq1, freq2, freq3;
 wire sigctr, rawsigin, sigin;
 signalinput sin (
 	.sysclk(sysclk), 
-	.regesetb(rst), 
+	.resetb(rst), 
 	.testmode(testmode), 
 	.sigin(rawsigin)
 	);
 
-rangechoose chooser(.range(range), .rawsigin(rawsigin), .sigin(sigin));
+rangechoose chooser(.range(range), .rawsigin(rawsigin), .sigin(sigin), .rst(rst));
 
 sigcontrol control(.sysclk(sysclk), .rst(rst), .sigctr(sigctr));
 
-wire enable, zero, lock;
-sig_en e(.clk(sigctr), .enable(enable), .zero(zero), .lock(lock), .rst(rst));
+wire enable, lock;
+sig_en e(.clk(sigctr), .enable(enable), .lock(lock), .rst(rst));
 
-wire times, lockedtimes;
+wire [20:0] times, lockedtimes;
 counter cnt0(
-	.clk(sigctr), 
 	.sigin(sigin), 
 	.times(times), 
 	.rst(rst), 
-	.enable(enable),
-	.zero(zero)
+	.enable(enable)
 	);
 
-locker lock0(.clk(sigctr), .din(times), .dout(lockedtimes), .rst(rst));
+locker lock0(.din(times), .dout(lockedtimes), .rst(rst), .lock(lock));
 
 wire dispLED;
 freqdisp disp0(
 	.din(lockedtimes), 
-	.clk(sigctr),
 	.freq0(freq0), .freq1(freq1), .freq2(freq2), .freq3(freq3),
 	.range(range),
 	.rangedisp(dispLED)
@@ -43,9 +40,9 @@ freqdisp disp0(
 
 endmodule
 
-module sig_en(clk, enable, zero, lock, rst);
+module sig_en(clk, enable, lock, rst);
 input clk, rst;
-output enable, zero, lock;
+output enable, lock;
 reg enable, zero;
 always @(posedge clk or negedge rst) begin
 	if (~rst) begin
@@ -55,86 +52,85 @@ always @(posedge clk or negedge rst) begin
 		enable <= ~enable;
 	end
 end
-assign zero = ~enable;
 assign lock = ~enable;
 endmodule
 
-module counter(clk, sigin, times, rst, enable, zero);
-input clk, sigin, rst, enable, zero;
+module counter(sigin, times, rst, enable);
+input sigin, rst, enable;
 output [20:0] times;
 reg [20:0] times; 
-reg enable;
-always @(posedge sigin or negedge rst or enable or zero) begin
+always @(posedge sigin or negedge rst or enable) begin
 	if (~rst) begin
 		times <= 0;
 	end
-	else if(enable) begin
+	else if(enable && sigin) begin
 		times <= times + 1;
 	end
 	else if(!enable) begin
-		if(zero) times <= 0;
+		times <= 0;
 	end
 end
 endmodule
 
-module locker(clk, din, dout, lock, rst);
-input clk;
+module locker(din, dout, lock, rst);
+input lock, rst;
 input [20:0] din;
 output [20:0] dout;
 reg [20:0] dout;
-always @(posedge clk or negedge rst or lock) begin
+always @(*) begin
 	if (~rst) begin
 		dout <= 21'd0;
 	end
 	else begin
-		if(lock) dout <= din;
+		if(!lock) dout <= din;
 	end
 end
 endmodule
 
 module freqdisp(din, freq0, freq1, freq2, freq3, range, rangedisp);
 input [20:0] din;
+input range;
 output [3:0] freq0, freq1, freq2, freq3;
 output rangedisp;
-wire num0, num1, num2, num3;
-assign num3 <= din / 1000;
-assign num2 <= (din - num3 * 1000) / 100;
-assign num1 <= (din - num3 * 1000 - num2 * 100) / 10;
-assign num0 <= (din - num3 * 1000 - num2 * 100 - num1 * 10);
+reg [3:0] num0, num1, num2, num3;
+always @(*) begin
+	num3 = din / 1000;
+	num2 = (din - num3 * 1000) / 100;
+	num1 = (din - num3 * 1000 - num2 * 100) / 10;
+	num0 = (din - num3 * 1000 - num2 * 100 - num1 * 10);
+end
 BCD7 B0(num0, freq0);
-BCD7 B0(num1, freq1);
-BCD7 B0(num2, freq2);
-BCD7 B0(num3, freq3);
-assign rangedisp <= range;
+BCD7 B1(num1, freq1);
+BCD7 B2(num2, freq2);
+BCD7 B3(num3, freq3);
+assign rangedisp = range;
 endmodule
 
 
-module rangechoose(range, rawsigin, sigin);
-input range, rawsigin;
+module rangechoose(range, rawsigin, sigin, rst);
+input range, rawsigin, rst;
 output sigin;
 reg sigin;
 reg [3:0] cnt;
 always @(*) begin
 	if(range == 0) sigin <= rawsigin;
+end
+always @(posedge rawsigin or negedge rst or range) begin
+	if (~rst) begin
+		cnt <= 4'd0;
+		sigin <= 0;
+	end
 	else begin
-		always @(posedge clk or negedge rst) begin
-			if (~rst) begin
-				cnt <= 4'd0;
-				sigin <= 0;
+		if(range == 1) begin
+			if(cnt == 4'd5) begin
+				cnt <= 4'd1;
+				sigin <= ~sigin;
 			end
-			else begin
-				if(cnt == 4'd5) begin
-					cnt <= 4'd1;
-					sigin <= ~sigin;
-				end
-				else cnt <= cnt + 4'd1;
-			end
+			else cnt <= cnt + 4'd1;
 		end
 	end
 end
-
 endmodule
-
 
 
 
@@ -142,19 +138,19 @@ endmodule
 module sigcontrol(sigctr, sysclk, rst);
 input sysclk,rst;
 output sigctr;
-wire sigctr;
-reg [20:0] cntctr;
-always @(posedge clk or negedge rst) begin
+reg sigctr;
+reg [31:0] cntctr;
+always @(posedge sysclk or negedge rst) begin
 	if (~rst) begin
 		cntctr <= 21'd0;
 		sigctr <= 0;
 	end
 	else begin
-		if (cntctr == 21'd25000000) begin
-			cntctr <= 21'd1;
+		if (cntctr == 32'd25000000) begin
+			cntctr <= 32'd1;
 			sigctr <= ~sigctr;
 		end
-		else cntctr <= cntctr + 21'd1;	//count++
+		else cntctr <= cntctr + 32'd1;	//count++
 	end
 end
 endmodule
@@ -166,7 +162,7 @@ testmode : 00,01,10,11 分别代表 3125,6250,50,12500Hz,使用 SW1~SW0 来控�
 */
 module signalinput( 
 	sysclk,
-	regesetb, 
+	resetb, 
 	testmode, 
 	sigin
 );
@@ -196,14 +192,15 @@ begin
 		state <= 21'd0; 
 	end
 	else begin
-	if(state==divide)
+	if(state==divide - 2)
 		state <= 21'd0;
 	else
 		state <= state + 21'd2;
-	if(state==21’d0) 
+	if(state==21'd0) 
 		sigin <= ~sigin;
 	end 
 end
+endmodule
 
 module BCD7(din,dout);
 input [3:0] din;
@@ -219,7 +216,5 @@ assign  dout=(din==4'h0)?7'b1000000:
              (din==4'h7)?7'b1111000:
              (din==4'h8)?7'b0000000:
              (din==4'h9)?7'b0010000:7'b0;
-endmodule
-
 endmodule
 
--- freqcnt_tb.v
+++ freqcnt_tb.v
@@ -4,11 +4,18 @@ reg sysclk, rst, range;
 reg [1:0] testmode;
 wire rangedisp;
 wire [3:0] freq0, freq1, freq2, freq3;
+freqcnt f0(
+	.sysclk(sysclk), .rst(rst), .range(range),
+	.testmode(testmode), 
+	.freq0(freq0), .freq1(freq1), .freq2(freq2), .freq3(freq3),
+	.rangedisp(rangedisp)
+	);
 initial begin
 sysclk = 0;
 rst = 0;
-range = 0;
-testmode = 2'b10
+range = 1;
+testmode = 2'b11;//12500Hz
 #130 rst = 1;
 end
-always #20 sysclk <= ~sysclk;
+always #10 sysclk <= ~sysclk;
+endmodule
